ba38a42533bdcc7d8335f4f183486d9b6161b270
Inggi Ramadhani <ramadhaniinggi@gmail.com>
2020-06-02 16:57:54 +0800

    keseluruhan dasar website untuk idcamp dicoding

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+function icFunction() {
+    let x = document.getElementById("menu");
+    if (x.className === "menu") {
+        x.className += " responsive";
+    } else {
+        x.className = "menu";
+    }
+}
+
+
+
+
+
